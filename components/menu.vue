f53abd8cba56bea3f8dce6f5a34ec43fa74731e8
Sonia-Brl <brunelsonia@gmail.com>
2025-01-09 23:45:15 +0100

    Init fetching github repos

--- components/menu.vue
+++ components/menu.vue
@@ -2,6 +2,9 @@
   <nav class="font-mono">
     <ul class="flex space-x-4">
       <li>
+        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+      </li>
+      <li>
         <NuxtLink to="/about" class="link">About</NuxtLink>
       </li>
       <li>
---
+++ components/project-list.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Project list component</div>
+  <section v-if="status === 'pending'">Loading...</section>
+  <section v-else-if="error">Something went wrong ... Try again!</section>
+  <section v-else>We will display the repos!</section>
+</template>
+
+<script setup>
+const {
+  error,
+  status,
+  data: gitProjects,
+} = await useFetch("https://api.github.com/users/Sonia-Brl/repos");
+console.log("data :", gitProjects);
+</script>
