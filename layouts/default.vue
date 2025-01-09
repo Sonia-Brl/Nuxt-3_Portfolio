855e8e588c1134b3bdbd798c32bc9c2ca77fcb48
Sonia-Brl <brunelsonia@gmail.com>
2025-01-09 23:17:32 +0100

    Add Google font

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,5 +20,23 @@ useHead({
       ? `${productCategory} - Site Title`
       : 'Site Title'
   } */
+
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+      crossorigin: "",
+    },
+  ],
 });
 </script>
+
+<style>
+body {
+  font-family: "Roboto";
+}
+</style>
