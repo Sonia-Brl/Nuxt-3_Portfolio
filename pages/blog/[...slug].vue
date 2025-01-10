befd6568290f3c807ad9171636192f5f98b94983
Sonia-Brl <brunelsonia@gmail.com>
2025-01-10 23:41:01 +0100

    Init table of content

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,7 +2,10 @@
   <article
     class="max-w-none prose dark:prose-invert prose-pre:bg-gray-200 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
   >
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      {{ doc.body.toc.links }}
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
   </article>
 </template>
 <script setup>
