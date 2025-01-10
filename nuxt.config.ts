b2750b4d336a23d0ab64e935289730da0cd4f0a7
Sonia-Brl <brunelsonia@gmail.com>
2025-01-10 22:12:59 +0100

    Add content theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/color-mode",
-    "@nuxt/content",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
   future: {
     compatibilityVersion: 4,
+  },
+  content: {
+    highlight: {
+      theme: {
+        default: "min-light",
+        dark: "min-dark",
+      },
+    },
+  },
+  colorMode: {
+    classSuffix: "",
   },
   app: {
     head: {
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,7 @@
 <template>
-  <article class="prose dark:prose-invert">
+  <article
+    class="max-w-none prose dark:prose-invert prose-pre:bg-gray-200 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
+  >
     <ContentDoc />
   </article>
 </template>
