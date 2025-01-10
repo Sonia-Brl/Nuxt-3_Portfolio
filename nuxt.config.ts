d36e5ee661484f2c0919ccc46e1dc3b2c376ac69
Sonia-Brl <brunelsonia@gmail.com>
2025-01-10 20:55:29 +0100

    Init Markdown content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+  ],
   future: {
     compatibilityVersion: 4,
   },
